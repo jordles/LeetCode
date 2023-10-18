8bc50c437ec1a8e753fda245a304a3e872c7fa4e
Jacky Yang <jackyyang1670@gmail.com>
2023-10-17 21:17:07 -0700

    Time: 61 ms (67.28%), Space: 41.7 MB (81.64%) - LeetHub

---
+++ 2862-interval-cancellation/2862-interval-cancellation.js
@@ -0,0 +1,45 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+    //setInterval will always call the function only after the interval, so we call function first.
+    let cancelFn = () => clearInterval(timer); //When we cancel, we clear the timer.
+    return cancelFn; //returning cancelFn will make the promise cancellable. 
+
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 35, cancelT = 190
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start)
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *   
+ *  setTimeout(() => {
+ *    console.log(result)  // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":35,"returned":8},
+ *                         //      {"time":70,"returned":8},           
+ *                         //      {"time":105,"returned":8},
+ *                         //      {"time":140,"returned":8},
+ *                         //      {"time":175,"returned":8}
+ *                         //  ]
+ *  }, cancelT + t + 15)    
+ */
